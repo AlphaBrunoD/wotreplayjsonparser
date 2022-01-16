328de81de2af39205e2c8509f02426f6eca3bfbd
AlphaBrunoD <96793443+AlphaBrunoD@users.noreply.github.com>
2022-01-16 11:21:45 +0300

    Add files via upload

--- WotReplayJSONParser.py
+++ WotReplayJSONParser.py
@@ -7,7 +7,7 @@ path='to %replays% folder'      #get sure folder contains only replays
 
 
 def get_replays():         #parse all replays in folder. 
-    n=0
+    n = 0
     for replay in os.listdir(path):
         relpay_read(replay)
         print(str(n)+' \ '+str(len(os.listdir(path)))+'   '+replay) #show progress
@@ -16,18 +16,17 @@ def get_replays():         #parse all replays in folder.
 def relpay_read(replay):
     f = open(path+replay, 'rb')
     d = open("path to destination.txt", 'a', encoding='utf-8')
-    skip= f.read(8)                               #skip 8 non-informative bytes
-    byteslen= f.read(4)
+    skip = f.read(8)                               #skip 8 non-informative bytes
+    byteslen = f.read(4)
     length = int.from_bytes(byteslen, 'little')   #very cool func, love it. gets decimal without extra convertations
     print(length)
-    info=f.read(length).decode()                  
-    infojson=json.loads(info)
-    players= infojson['vehicles'].values()
+    info = f.read(length).decode()                  
+    infojson = json.loads(info)
+    players = infojson['vehicles'].values()
     for player in players:                      #data processing example
-        name=player['name']
+        name = player['name']
         d.write("'"+name+"', ")
-        # print(name)
-
+    d.close()
     f.close()
 
 def main():

503baaad39a4eada98da12531fd9239bc53b8411
AlphaBrunoD <96793443+AlphaBrunoD@users.noreply.github.com>
2022-03-20 20:18:13 +0300

    Add files via upload

--- WotReplayJSONParser.py
+++ WotReplayJSONParser.py
@@ -4,27 +4,26 @@ import json
 path = 'A:\\Games\\World_of_Tanks_RU\\replays\\'      #get sure folder contains only replays
 
 def get_replays():                #parse all replays in folder. 
-    n = 0
-    for replay in os.listdir(path):
-        relpay_read(replay)
-        print(str(n) + ' \ ' + str(len(os.listdir(path))) + '   ' + replay) #show progress
-        n += 1
+    w_path = os.listdir(path)
+    for n in range(0, len(w_path)):
+        replay_read(w_path[n])
+        print(f"{n} \ {len(w_path)}   {w_path[n]}")
 
-def relpay_read(replay):
-    f = open(path + replay, 'rb')
-    d = open("path_to_destination.txt", 'a', encoding='utf-8')
-    skip = f.read(8)                               #skip 8 non-informative bytes
-    byteslen = f.read(4)
+def replay_read(replay):
+    file = open(path + replay, 'rb')
+    dest = open("path_to_destination.txt", 'a', encoding='utf-8')
+    skip = file.read(8)                               #skip 8 non-informative bytes
+    byteslen = file.read(4)
     length = int.from_bytes(byteslen, 'little')    #very cool func, love it. gets decimal without extra convertations
     print(length)
-    info = f.read(length).decode()                  
+    info = file.read(length).decode()                  
     infojson = json.loads(info)
     players = infojson['vehicles'].values()        #data processing example
     for player in players:                      
         name = player['name']
-        d.write("'" + name + "', ")
-    d.close()
-    f.close()
+        dest.write("'" + name + "', ")
+    dest.close()
+    file.close()
 
 def main():
     get_replays()
